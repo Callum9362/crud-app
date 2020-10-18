7581837ec3dfad33e2d4e881568b227b65f3980f
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-10-18 17:58:48 +1100

    Added some temporary middleware

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 const PORT = 3000;
 
 //Import our routes
-const routes = require('./src/routes/crmRoutes')
-routes(app);
+//const routes = require('./src/routes/crmRoutes')
+//routes(app);
+
+app.get('/', function(req, res, next){
+    console.log('Req Method: ', req.method)
+    next();
+}, function(req, res, next){
+    console.log('Request Original Url', req.originalUrl);
+    next();
+}, function(req, res, next){
+    res.send('Request was successful');
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
